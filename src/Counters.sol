640c8eccba39816d75401c4496f6101b8fc87b39
atharva <0xAtharva@protonmail.com>
2024-02-01 14:34:34 +0530

    transparent proxy added

--- src/Counters.sol
+++ src/Counters.sol
@@ -1,9 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {ERC1967Proxy} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {Clones} from "openzeppelin-contracts/proxy/Clones.sol";
-import {ERC1967Utils} from "openzeppelin-contracts/proxy/ERC1967/ERC1967Utils.sol";
+import "openzeppelin-contracts/proxy/Clones.sol";
+import "openzeppelin-contracts/proxy/utils/Initializable.sol";
+
+import "openzeppelin-contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import "openzeppelin-contracts/proxy/ERC1967/ERC1967Utils.sol";
+
+import "openzeppelin-contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts/proxy/transparent/ProxyAdmin.sol";
+
+import "openzeppelin-contracts/proxy/utils/UUPSUpgradeable.sol";
 
 contract Counter1 {
     uint256 public x;
@@ -19,25 +26,23 @@ contract Counter1 {
 
 contract Counter2 {
     uint256 public x;
+    uint256 public y;
 
     function setX(uint256 newNumber) public {
         x = newNumber;
     }
 
-    function increment() public {
+    function increment_x() public {
         x++;
         x++;
     }
-}
-
-contract CounterUpgradable is ERC1967Proxy {
-    uint256 public x;
-    constructor(address implementation,bytes memory data) ERC1967Proxy(implementation,data) { }
-
-    function upgrade(address _newImplementation) public {
-        ERC1967Utils.upgradeToAndCall(_newImplementation,"");
+    function increment_y() public {
+        y++;
+        y++;
     }
-
+    function total() view public returns(uint256) {
+        return x+y;
+    }
 }
 
 contract Counter1Factory {
@@ -49,4 +54,24 @@ contract Counter1Factory {
         return address(c).clone();
     }
 
+}
+
+contract Counter1967Proxy is ERC1967Proxy {
+    uint256 public x;
+    constructor(address implementation,bytes memory data) ERC1967Proxy(implementation,data) { }
+
+    function upgrade(address _newImplementation) public {
+        ERC1967Utils.upgradeToAndCall(_newImplementation,"");
+    }
+
+}
+
+contract CounterTransparent is TransparentUpgradeableProxy {
+    uint256 public x;
+
+    constructor(address _logic, address initialOwner, bytes memory _data) TransparentUpgradeableProxy(_logic,initialOwner,_data) {}
+
+    function whoAdmin() public returns(address){
+        return _proxyAdmin();
+    }
 }
--- test/Counters.t.sol
+++ test/Counters.t.sol
@@ -2,39 +2,28 @@
 pragma solidity ^0.8.13;
 
 import {Test, console2} from "forge-std/Test.sol";
-import {Counter1, Counter2, CounterUpgradable, Counter1Factory} from "../src/Counters.sol";
+import {Counter1, Counter2, Counter1Factory, Counter1967Proxy, CounterTransparent} from "../src/Counters.sol";
+import "openzeppelin-contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "openzeppelin-contracts/proxy/transparent/ProxyAdmin.sol";
+
 
 contract CounterTest is Test {
+    address deployer = address(0x7FA9385bE102ac3EAc297483Dd6233D62b3e1496);
     Counter1 public c1;
     Counter2 public c2;
     Counter1Factory public f1;
-    CounterUpgradable public cUp;
+    Counter1967Proxy public c1967Proxy;
+    CounterTransparent public cT;
 
     function setUp() public {
         c1 = new Counter1();
         c2 = new Counter2();
         f1 = new Counter1Factory();
-        cUp = new CounterUpgradable(address(c1),"");
+        c1967Proxy = new Counter1967Proxy(address(c1),"");
+        cT = new CounterTransparent(address(c1),deployer,"");
     }
 
-    function test_CounterUpgradable() public {
-        (bool ok,) = address(cUp).call(abi.encodeWithSignature("increment()"));
-        require(ok,"increment() failed");
-        (,bytes memory data) = address(cUp).call(abi.encodeWithSignature("x()"));
-        uint256 x = uint256(bytes32(data));
-        assertEq(x, 1);
-
-        (bool upgraded,) = address(cUp).call(abi.encodeWithSignature("upgrade(address)",address(c2)));
-        require(upgraded,"upgrade to c2 failed");
-        
-        (bool ok1,) = address(cUp).call(abi.encodeWithSignature("increment()"));
-        require(ok1,"increment() failed");
-        (,bytes memory data1) = address(cUp).call(abi.encodeWithSignature("x()"));
-        uint256 y = uint256(bytes32(data1));
-        assertEq(y, 3);
-    }
-
-    function test_Increment() public {
+    function test_Clones() public {
         Counter1 c = Counter1(f1.getNewCounter());
         assertEq(c.x(), 0);
         c.increment();
@@ -42,5 +31,44 @@ contract CounterTest is Test {
         c.setX(100);
         assertEq(c.x(), 100);
     }
+
+    function test_Counter1967Proxy() public {
+        (bool ok,) = address(c1967Proxy).call(abi.encodeWithSignature("increment()"));
+        require(ok,"increment() failed");
+        (,bytes memory data) = address(c1967Proxy).call(abi.encodeWithSignature("x()"));
+        uint256 x = uint256(bytes32(data));
+        assertEq(x, 1);
+
+        (bool upgraded,) = address(c1967Proxy).call(abi.encodeWithSignature("upgrade(address)",address(c2)));
+        require(upgraded,"upgrade to c2 failed");
+        
+        (bool ok1,) = address(c1967Proxy).call(abi.encodeWithSignature("increment_x()"));
+        require(ok1,"increment() failed");
+        address(c1967Proxy).call(abi.encodeWithSignature("increment_y()"));
+        (,bytes memory total) = address(c1967Proxy).call(abi.encodeWithSignature("total()"));
+        uint256 amt = uint256(bytes32(total));
+        assertEq(amt, 5);
+    }
+
+    function test_CounterTransparent() public {
+        assertEq(cT.x(), 0);
+        (bool ok,) = address(cT).call(abi.encodeWithSignature("increment()"));
+        require(ok,"increment() failed");
+        (,bytes memory data) = address(cT).call(abi.encodeWithSignature("x()"));
+        uint256 x = uint256(bytes32(data));
+        assertEq(x, 1);
+
+        address admin = cT.whoAdmin();
+        vm.prank(deployer);
+        (bool upgraded,) = admin.call(abi.encodeWithSelector(ProxyAdmin.upgradeAndCall.selector, ITransparentUpgradeableProxy(address(cT)),address(c2),""));
+        require(upgraded,"upgrade to c2 failed");
+        
+        (bool ok1,) = address(cT).call(abi.encodeWithSignature("increment_x()"));
+        require(ok1,"increment() failed");
+        address(cT).call(abi.encodeWithSignature("increment_y()"));
+        (,bytes memory total) = address(cT).call(abi.encodeWithSignature("total()"));
+        uint256 amt = uint256(bytes32(total));
+        assertEq(amt, 5);
+    }    
 }
 
